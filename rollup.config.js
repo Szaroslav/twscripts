2b189a68e50fa982009461dd81f8ff164109794c
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-09-14 20:16:53 +0200

    Generate banner comments

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
+import { readFileSync } from 'node:fs';
 import { globSync } from 'glob';
+import stripCode from 'rollup-plugin-strip-code';
 
 export default () => {
+  const bannerCommentRegExp = /^\/\*\*.*\*\//s;
   const inputFilenames = globSync('./src/*.js', { dotRelative: true }).map(filePath => {
     const pathSegments = filePath.split(/\/|\\/);
     const filename = pathSegments[pathSegments.length - 1];
@@ -11,7 +14,22 @@ export default () => {
   return inputFilenames.map(filename => ({
     input: `./src/${filename}.js`,
     output: {
-      file: `./dist/${filename}.js`
-    }
+      file: `./dist/${filename}.js`,
+      format: 'iife',
+      banner: chunkInfo => {
+        const fileContent = readFileSync(chunkInfo.facadeModuleId, { encoding: 'utf-8' });
+
+        let banner = '';
+        if (bannerCommentRegExp.test(fileContent)) {
+          banner = fileContent.match(bannerCommentRegExp);
+        }
+        return banner + '\n';
+      }
+    },
+    plugins: [
+      stripCode({
+        pattern: bannerCommentRegExp
+      })
+    ]
   }));
 };

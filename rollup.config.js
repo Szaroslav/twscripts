a812dfdd5f79d7e624dd5e4a411b27230583ebbb
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-10-01 10:15:08 +0200

    Add `rollup-plugin-handlebars-plus` module

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
-import { readFileSync } from 'node:fs';
-import { globSync } from 'glob';
-import stripCode from 'rollup-plugin-strip-code';
+import { readFileSync } from "node:fs";
+import { globSync } from "glob";
+import stripCode from "rollup-plugin-strip-code";
+import handlebars from "rollup-plugin-handlebars-plus";
 
 export default () => {
   const bannerCommentRegExp = /^\/\*\*.*\*\//s;
-  const inputFilenames = globSync('./src/*.js', { dotRelative: true }).map(filePath => {
+  const inputFilenames = globSync("./src/*.js", { dotRelative: true }).map(filePath => {
     const pathSegments = filePath.split(/\/|\\/);
     const filename = pathSegments[pathSegments.length - 1];
-    const filenameWithoutExtension = filename.slice(0, filename.lastIndexOf('.'));
+    const filenameWithoutExtension = filename.slice(0, filename.lastIndexOf("."));
     return filenameWithoutExtension;
   });
 
@@ -15,21 +16,23 @@ export default () => {
     input: `./src/${filename}.js`,
     output: {
       file: `./build/${filename}.js`,
-      format: 'iife',
+      format: "iife",
       banner: chunkInfo => {
-        const fileContent = readFileSync(chunkInfo.facadeModuleId, { encoding: 'utf-8' });
+        const fileContent = readFileSync(chunkInfo.facadeModuleId, { encoding: "utf-8" });
 
-        let banner = '';
+        let banner = "";
         if (bannerCommentRegExp.test(fileContent)) {
           banner = fileContent.match(bannerCommentRegExp);
         }
-        return banner + '\n';
+        return banner + "\n";
       }
     },
+    external: [ "handlebars" ],
     plugins: [
       stripCode({
         pattern: bannerCommentRegExp
-      })
+      }),
+      handlebars()
     ]
   }));
 };

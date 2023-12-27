c91b07a4e5d4fe24f785c4d592b48a02fdec67e8
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-12-27 23:41:10 +0100

    Unarchive schedule merger and common module `Memo`

--- src/common/Memo.js
+++ src/common/Memo.js
@@ -1,17 +1,3 @@
-/**
- * ScheduleMerger.js v0.9
- * Szary (Plemiona: AGH Szary)
- * GitHub: https://github.com/Szaroslav
- * 
- * Skrypt łączący kilka rozpisek z plemiona-planer.pl w jedną, sortujący od najwcześniejszych do najpóźniejszych rozkazów,
- * jeśli rozpiska jest zbyt duża, skrypt dzieli je na kilka notatek.
- * Dopuszczalne formaty rozpisek: tekst prosty, rozszerzony lub dla zastąpcy - skrypt aktualnie nie obsługuje tabel.
- */
-
-// import _Memo from './common/Memo.js';
-
-(function () {
-
 const _Memo = {
     MAX_MEMO_SIZE: typeof char_limit === 'number' ? char_limit : 6e4,
     MSG_DURATION: 1400,
@@ -60,7 +46,7 @@ const _Memo = {
         }
 
         Memo.mobile && Memo.checkArrow(Memo.tabs.length);
-        
+
         return $.ajax({
             url: $("#add_tab_url").val(),
             type: "POST",
@@ -88,7 +74,7 @@ const _Memo = {
             },
         }).then(res => this.createdMemoIds.push(res.id));
     },
-    
+
     renameTab: function (title) {
         const memo = Memo.tabs[Memo.findTab(Memo.selectedTab)];
         memo.title = title;
@@ -128,7 +114,7 @@ const _Memo = {
     splitSchedule: function (schedule) {
         if (schedule instanceof Array === false)
             return null;
-        
+
         const newSchedule = [];
         const newLine = '\r\n';
         let memoText = '';
@@ -149,50 +135,4 @@ const _Memo = {
     }
 };
 
-const ScheduleMerger = {
-    MSG_DURATION: 1400,
-
-    init: function () {
-        console.log('%cScheduleMerger.js %cv0.9', 'display: inline-block; padding: 4px 0', 'display: inline-block; padding: 4px; background-color: #2151ae; color: white');
-        console.log('Skrypt stworzony przez %cSzary %c(Plemiona: %cAGH Szary%c)', 'font-weight: bold', 'font-weight: normal', 'font-weight: bold', 'font-weight: normal');
-
-        if (typeof Memo !== 'undefined') {
-            this.MSG_DURATION = _Memo.MSG_DURATION;
-            const schedule = this.getSchedule();
-            
-            _Memo.create(schedule);
-        }
-        else {
-            UI.ErrorMessage('Nie jesteś w notatkach. Przenoszę.');
-            setTimeout(() => location.href = `${location.origin}/game.php?screen=memo`, this.MSG_DURATION + 600);
-        }
-    },
-
-
-
-    getSchedule: function () {
-        const schedule = [];
-        const memos = document.querySelectorAll('.memo_container');
-
-        memos.forEach(memo => {
-            const d = _Memo.getSchedule(memo.querySelector('textarea[name="memo"]').value, 'text');
-            for (let i = 0; i < d.length; i += 3) {
-                if (i + 2 >= d.length) return;
-                schedule.push([d[i], d[i + 1], d[i + 2]]);
-            }
-        });
-
-        schedule.sort((a, b) => {
-            const dateA = new Date(`${a[1].match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}T${a[1].match(/[0-9]{2}:[0-9]{2}:[0-9]{2}/)}`);
-            const dateB = new Date(`${b[1].match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)}T${b[1].match(/[0-9]{2}:[0-9]{2}:[0-9]{2}/)}`);
-            return dateA - dateB;
-        });
-        schedule.forEach((order, i) => order[0] = order[0].replace(/^[0-9]{1,}./, `${i + 1}.`))
-
-        return schedule;
-    }
-};
-
-ScheduleMerger.init();
-
-})();
+export default _Memo;

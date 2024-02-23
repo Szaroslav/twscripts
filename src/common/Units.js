a94771071e5fa41c2f2ba5d0e9b76e7b3361bd18
Jakub Szaredko <54867641+Szaroslav@users.noreply.github.com>
2024-02-23 22:14:37 +0100

    Units: Add documentation comments (#28)

--- src/common/Units.js
+++ src/common/Units.js
@@ -1,5 +1,11 @@
 export default class Units {
   /**
+   * Defines custom options.
+   * @typedef {object} UnitOptions
+   * @property {number} [cache]
+   */
+
+  /**
    * Defines the properties of the particular unit type.
    * @typedef {Object} UnitData
    * @property {number} recruitmentTimeSec
@@ -21,12 +27,18 @@ export default class Units {
    */
   #cache;
 
+  /**
+   * @param {UnitOptions} [options = {}]
+   */
   constructor(options = {}) {
    this.#cache = typeof options.cache === "number"
       ? 1000 * options.cache
       : 0;
   }
 
+  /**
+   * Fetches information about units from the remote configuration.
+   */
   async fetch() {
     const url           = `${window.location.origin}/interface.php?func=get_unit_info`;
     const response      = await fetch(url),
@@ -42,6 +54,12 @@ export default class Units {
     }
   }
 
+  /**
+   * Processes all data of certain unit type.
+   * @param {string} response - A fetched configuration.
+   * @param {number} i - Current line position of processing.
+   * @returns {number} Line position after processing.
+   */
   #processUnitData(response, i) {
     const unitName = this.#getOpeningMarkupName(response[i++]);
     this.data[unitName] = {};
@@ -89,6 +107,14 @@ export default class Units {
     return i;
   }
 
+  /**
+   * Verifies, whether processing of certain unit type is finished.
+   * Line and markup name must be a string
+   * and markup name must be different than `unitName`.
+   * @param {string | undefined} line - Currently processed line.
+   * @param {string} unitName - Currently processed unit name.
+   * @returns {boolean}
+   */
   #isUnitDataOver(line, unitName) {
     let markupName;
     return line
@@ -96,6 +122,12 @@ export default class Units {
       && typeof markupName === "string";
   }
 
+  /**
+   * Gets a name of markup, based on mode.
+   * @param {string} markup - Markup from which extracting the name.
+   * @param {"opening" | "closing" | "any"} [mode = "any"] - Desired markup type.
+   * @returns {string | null} - The first occurance of markup name, null otherwise.
+   */
   #getMarkupName(markup, mode = "any") {
     let markupRegex = /<\/?(.*?)>/;
     if (mode === "opening") {
@@ -109,10 +141,20 @@ export default class Units {
     return (markupMatch && markupMatch[1]) ?? null;
   }
 
+  /**
+   * Gets a name of opening markup.
+   * @param {string} markup - Markup from which extracting the name.
+   * @returns {string | null} - The first occurance of opening markup name, null otherwise.
+   */
   #getOpeningMarkupName(markup) {
     return this.#getMarkupName(markup, "opening");
   }
 
+  /**
+   * Gets a name of closing markup.
+   * @param {string} markup - Markup from which extracting the name.
+   * @returns {string | null} - The first occurance of closing markup name, null otherwise.
+   */
   #getMarkupValue(markup) {
     const markupValueRegex = /<(.*?)>(.*?)<\/\1>/,
           markupMatch      = markup.match(markupValueRegex),
@@ -124,6 +166,9 @@ export default class Units {
     return !isNaN(Number(markupValue)) ? Number(markupValue) : markupValue;
   }
 
+  /**
+   * Saves the data into local storage.
+   */
   save() {
     localStorage.setItem("jsUnits:data", JSON.stringify({
       timestamp: Date.now(),
@@ -131,6 +176,9 @@ export default class Units {
     }));
   }
 
+  /**
+   * Load the data from local storage.
+   */
   async load() {
     try {
       const cache                        = this.#cache;
